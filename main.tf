78f116178152cec12090dffb28e469112feecd99
Marcel Wiget <mwiget@gmail.com>
2022-10-23 10:13:12 +0200

    combine into main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,39 @@
+module "spoke_vnet_a" {
+  source                  = "./spoke_vnets"
+  azure_region            = var.azure_region
+  azure_az                = "1"
+  vnet_name               = "${var.project_prefix}-spoke-vnet-a"
+  vnet_cidr_block         = "10.32.16.0/22"
+  vnet_subnet_cidr_block  = "10.32.16.0/24"
+  instance_template_data  = {
+    tailscale_key   = var.tailscale_key,
+    tailscale_hostname = format("%s-spoke-vnet-a", var.project_prefix)
+  }
+  custom_tags             = {
+    Name  = "${var.project_prefix}-spoke-vnet-a"
+    Owner = var.owner_tag
+  }
+  ssh_public_key          = var.ssh_public_key
+}
+
+module "spoke_vnet_b" {
+  source                  = "./spoke_vnets"
+  azure_region            = var.azure_region
+  azure_az                = "2"
+  vnet_name               = "${var.project_prefix}-spoke-vnet-b"
+  vnet_cidr_block         = "10.32.20.0/22"
+  vnet_subnet_cidr_block  = "10.32.20.0/24"
+  instance_template_data  = {
+    tailscale_key   = var.tailscale_key,
+    tailscale_hostname = format("%s-spoke-vnet-b", var.project_prefix)
+  }
+  custom_tags             = {
+    Name  = "${var.project_prefix}-spoke-vnet-b"
+    Owner = var.owner_tag
+  }
+  ssh_public_key          = var.ssh_public_key
+}
+
 module "azure-site-1" {
   source                              = "./modules/f5xc/site/azure"
   f5xc_namespace                      = "system"
@@ -51,4 +87,10 @@ module "azure-site-1" {
 
 output "azure-site-1" {
   value = module.azure-site-1
+}
+output "spoke_vnet_a" {
+  value = module.spoke_vnet_a
+}
+output "spoke_vnet_b" {
+  value = module.spoke_vnet_b
 }
--- provider.tf
+++ provider.tf
@@ -1,3 +1,12 @@
+provider "volterra" {
+  alias        = "default"
+  api_p12_file = var.f5xc_api_p12_file
+  api_cert     = var.f5xc_api_p12_file != "" ? "" : var.f5xc_api_cert
+  api_key      = var.f5xc_api_p12_file != "" ? "" : var.f5xc_api_key
+  api_ca_cert  = var.f5xc_api_ca_cert
+  url          = var.f5xc_api_url
+}
+
 provider "azurerm" {                                                                              
   subscription_id = var.azure_subscription_id != "" ? "" : var.azure_subscription_id
   client_id       = var.azure_client_id != "" ? "" : var.azure_client_id 
